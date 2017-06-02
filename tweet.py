3a5a2c191cf0113922b3235eb673a497100316eb
Neethu Mohandas <prayaganeethu@gmail.com>
2017-06-02 14:31:01 +0530

    Gets the tweets with links out of the latest 20 tweets in my twitter timeline, renders it in the browser

--- tweet.py
+++ tweet.py
@@ -3,7 +3,13 @@ try:
 except ImportError:
     from ConfigParser import ConfigParser  # ver. < 3.0
 import tweepy
+from flask import render_template, flash, redirect, Flask
 from tweepy import OAuthHandler
+import json
+from tweepy.parsers import JSONParser
+import re
+
+app = Flask(__name__)
 
 config = ConfigParser()
 
@@ -17,7 +23,29 @@ access_secret = config.get('twitter', 'access_secret')
 auth = OAuthHandler(consumer_key, consumer_secret)
 auth.set_access_token(access_token, access_secret)
 
+# api = tweepy.API(auth, parser=JSONParser())
+
 api = tweepy.API(auth)
 
-for status in tweepy.Cursor(api.home_timeline).items(10):
-    print(status.text)
+# text = []
+# url = []
+# count = 0
+
+statuses = tweepy.Cursor(api.home_timeline).items(20)
+# text[count] = status.text
+# urls = re.findall(
+#     'http[s]?://(?:[a-zA-Z]|[0-9]|[$-_@.&+]|[!*\(\),]|(?:%[0-9a-fA-F][0-9a-fA-F]))+', status.text)
+# url[count] = urls
+# count += 1
+
+# statuses = api.search(id="TechCrunch")
+# json_str = json.dumps(statuses)
+# for status in tweepy.Cursor(api.home_timeline).items(20):
+#     print(status)
+
+
+@app.route('/')
+def tweets():
+    return render_template("tweets.html", statuses=statuses)
+
+app.run(debug=True)
